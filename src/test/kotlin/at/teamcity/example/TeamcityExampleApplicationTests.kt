00aead9ca4765e34511b4c7d65a59f0c99dbd04e
Uygar Senocak <h50rzxa@s-mxs.net>
2020-10-13 19:01:57 +0200

    Test controller and test

--- src/test/kotlin/at/teamcity/example/TeamcityExampleApplicationTests.kt
+++ src/test/kotlin/at/teamcity/example/TeamcityExampleApplicationTests.kt
@@ -1,12 +1,26 @@
 package at.teamcity.example
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TeamcityExampleApplicationTests {
+
+    @Autowired
+    private val testRestTemplate = TestRestTemplate()
 
     @Test
     fun contextLoads() {
+        val response = testRestTemplate.exchange("/", HttpMethod.GET, null, String::class.java)
+        assertEquals(HttpStatus.OK,response.statusCode)
+        assertEquals("Hello World", response.body)
     }
 }
