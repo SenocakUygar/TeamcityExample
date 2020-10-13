5aa5ee88299c686bf74ad1d6dd147234aa4e0b6b
Uygar Senocak <h50rzxa@s-mxs.net>
2020-10-13 18:28:15 +0200

    Project

---
+++ src/main/kotlin/at/teamcity/example/TeamcityExampleApplication.kt
@@ -0,0 +1,11 @@
+package at.teamcity.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamcityExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamcityExampleApplication>(*args)
+}
---
+++ src/test/kotlin/at/teamcity/example/TeamcityExampleApplicationTests.kt
@@ -0,0 +1,12 @@
+package at.teamcity.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamcityExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
