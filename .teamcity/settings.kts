ff2bf881359d4fcf8e924a00e85d07637bddcf22
Uygar Senocak <h50rzxa@s-mxs.net>
2020-10-13 20:26:49 +0200

    Change build tyoe

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,11 +1,28 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 version = "2020.1"
 
 project {
     buildType(HelloWorld)
 }
+
+object BuildAndDeploy : BuildType({
+    name = "Build And Deploy"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        print("Maven clean test is runned!")
+        maven {
+            goals = "clean install"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+})
 
 object HelloWorld: BuildType({
     name = "Hello world"
