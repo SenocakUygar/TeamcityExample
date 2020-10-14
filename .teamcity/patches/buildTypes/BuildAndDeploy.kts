7dec8d5d3562fda50cc19c68ac3b6ca988274073
admin <admin@TeamCity>
2020-10-14 08:02:10 +0000

    TeamCity change in 'TeamcityExample' project: runners of 'Build And Deploy' build configuration were updated

--- .teamcity/patches/buildTypes/BuildAndDeploy.kts
+++ .teamcity/patches/buildTypes/BuildAndDeploy.kts
@@ -27,7 +27,7 @@ changeBuildType(RelativeId("BuildAndDeploy")) {
                 type = "ssh-deploy-runner"
                 param("jetbrains.buildServer.deployer.username", "h50rzxa@beeone.lan")
                 param("jetbrains.buildServer.deployer.sourcePath", "**/*.jar")
-                param("jetbrains.buildServer.deployer.targetUrl", "10.8.241.51/home/beeone.lan/h50rzxa/test")
+                param("jetbrains.buildServer.deployer.targetUrl", "{10.8.241.51}[:/home/beeone.lan/h50rzxa/test]")
                 param("secure:jetbrains.buildServer.deployer.password", "credentialsJSON:f9f9575b-b121-4d2c-9132-2eb2af23baf4")
                 param("jetbrains.buildServer.sshexec.authMethod", "PWD")
                 param("jetbrains.buildServer.deployer.ssh.transport", "jetbrains.buildServer.deployer.ssh.transport.scp")
