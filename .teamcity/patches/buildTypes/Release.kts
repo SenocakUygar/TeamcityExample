de29d153e6d8eb1b9c8c6a43f26cef9cec74c9af
admin <admin@TeamCity>
2020-10-16 09:43:03 +0000

    TeamCity change in 'TeamcityExample' project: runners of 'Release' build configuration were updated

--- .teamcity/patches/buildTypes/Release.kts
+++ .teamcity/patches/buildTypes/Release.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
 
 /*
@@ -15,6 +16,13 @@ create(DslContext.projectId, BuildType({
 
     vcs {
         root(RelativeId("HttpsGithubComUygarSenocakkTeamcityExampleGit"))
+    }
+
+    steps {
+        maven {
+            name = "Release"
+            goals = "release:prepare"
+        }
     }
 }))
 
